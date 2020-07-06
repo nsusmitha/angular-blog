da8b6061f02df33e96e6f147ae55fb99b0dec839
susmitha <49911847+nsusmitha@users.noreply.github.com>
2020-07-06 10:14:20 +1000

    remove console.log statements

--- src/app/add-blog/add-blog.component.ts
+++ src/app/add-blog/add-blog.component.ts
@@ -15,25 +15,17 @@ export class AddBlogComponent implements OnInit {
   public blog:Blog;
   constructor(private addBlogService:AddBlogService) {
   this.blog = new Blog();
-  }
+ }
    addblog()
    {
-    //console.log(addblogForm.value);
-    //console.log(this.blog);
-   //console.log(this.blog.title);
-   //console.log(this.blog.content);
    this.addBlogService.addBlog(this.blog);
   }
-  //  addblog(title,content){
-  //    console.log(title.value,content.value);
-  //    alert("blog submitted")
-  //  }
-    //constructor(){}
+  
     //blogs=[]
-  ngOnInit():void {
+ ngOnInit(){
      this.addblog();
   }
-    //addblog(title:any, content){
+    //addblog(title, content){
      //let blog={"title": title.value,"content":content.value};
    // if(localStorage.getItem("blogs")){
   //     this.blogs=JSON.parse(localStorage.getItem("blogs"))
